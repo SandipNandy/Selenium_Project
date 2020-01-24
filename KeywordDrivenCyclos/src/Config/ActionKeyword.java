e96d595eff9e03e223eb614ff39426bb732b106c
IBM <IBM@172.24.84.20>
2020-01-24 12:11:22 +0530

    selenium project

---
+++ KeywordDrivenCyclos/src/Config/ActionKeyword.java
@@ -0,0 +1,105 @@
+package Config;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class ActionKeyword {
+	public static WebDriver driver;
+	 
+	 public static void openBrowser(){ 
+		 
+		 System.setProperty("webdriver.chrome.driver", "E:\\Softwares Required For Testing Batch\\chromedriver_win32\\chromedriver.exe");
+	     driver=new ChromeDriver();
+	     driver.manage().window().maximize();
+	 }
+	 
+	 public static void navigate(){ 
+	 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	 driver.get("http://localhost:8585/do/login");
+	 }
+	 
+	/* public static void click_MyAccount(){
+	 driver.findElement(By.xpath(".//*[@id='account']/a")).click();
+	 }
+	 */
+	 public static void click_Admin(){
+	 driver.findElement(By.id("cyclosUsername")).sendKeys("admin");}
+	 
+	 public static void click_Password(){
+		 driver.findElement(By.xpath("//*[@class='virtualKeyboardButton virtualKeyboardContrastNormal' and @value='1']")).click();
+			driver.findElement(By.xpath("//*[@class='virtualKeyboardButton virtualKeyboardContrastNormal' and @value='2']")).click();
+			driver.findElement(By.xpath("//*[@class='virtualKeyboardButton virtualKeyboardContrastNormal' and @value='3']")).click();
+			driver.findElement(By.xpath("//*[@class='virtualKeyboardButton virtualKeyboardContrastNormal' and @value='4']")).click();
+		 }
+		 
+		 public static void Submit(){
+			 driver.findElement(By.xpath("//div[@id='virtualKeyboard']//div//input[@class='button']")).click();
+		 }
+		 
+		 public static void waitFor() throws Exception{
+		 Thread.sleep(5000);
+		 }
+		 
+		 public static void Account(){
+		 driver.findElement (By.xpath("//span[@class='menuText'][contains(text(),'Accounts')]")).click();
+		 }
+		 
+		 public static void Member_Payment(){
+			 driver.findElement (By.xpath("//span[contains(text(),'Member Payment')]")).click();
+			
+			 String  actualvalue=driver.findElement(By.xpath("//td[contains(text(),'Payment system to member')]")).getText();
+				String expectedvalue="Payment system to member";
+				Assert.assertEquals(actualvalue, expectedvalue);
+				System.out.println(actualvalue);	 
+		 }
+		 public static void Login_Recipint(){
+			 driver.findElement (By.xpath("//input[@id='memberUsername']")).sendKeys("Dhwani");
+			 }
+		 public static void Login_RecipintName(){
+			 driver.findElement (By.xpath("//input[@id='memberName']")).sendKeys("Dhwani");
+			 }
+		 
+		 public static void Amount(){
+			 driver.findElement (By.xpath("//input[@id='amount']")).sendKeys("10000");
+			 }
+		 public static void Description(){
+			 driver.findElement (By.xpath("//textarea[@id='description']")).sendKeys("system");
+			 }
+		 public static void Submit2(){
+			 driver.findElement (By.xpath("//input[@id='submitButton']")).click();
+			 
+			 String  actualvalue1=driver.findElement(By.xpath("//td[@class='label']")).getText();
+				String expectedvalue1="You are about to perform the following payment:";
+				Assert.assertEquals(actualvalue1, expectedvalue1);
+				System.out.println(actualvalue1);
+			 }
+		 public static void Submit3(){
+			 driver.findElement (By.xpath("//table[@class='defaultTable']//input[@class='button']")).click();
+		    
+			 String  actualvalue2=driver.findElement(By.xpath("//td[contains(text(),'Cyclos')]")).getText();
+				String expectedvalue2="Cyclos";
+				Assert.assertEquals(actualvalue2, expectedvalue2);
+				System.out.println(actualvalue2);
+		 
+		 }
+		 
+		 public static void LogOut() throws InterruptedException{
+			 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			 driver.findElement(By.xpath("//input[@id='btn']")).click();
+			 Thread.sleep(2000);
+			 driver.findElement(By.xpath("//span[contains(text(),'Logout')]")).click();
+			 Thread.sleep(2000);
+			 Alert alert1=driver.switchTo().alert();
+				alert1.accept();
+		 }
+		 
+		 public static void closeBrowser(){
+		 driver.quit();
+
+}
+	 }
---
+++ KeywordDrivenCyclos/src/executionEngine/DriverScripts.java
@@ -0,0 +1,72 @@
+package executionEngine;
+
+import Config.ActionKeyword;
+import Utility.ExcelUtils;
+
+public class DriverScripts {
+	
+	 public static void main(String[] args) throws Exception {
+	     // Declaring the path of the Excel file with the name of the Excel file
+	     String sPath = "E://New folder (3)//KeywordDrivenCyclos//src//dataEngine//DataEngine.xlsx";
+	 
+	     // Here we are passing the Excel path and SheetName as arguments to connect with Excel file 
+	     ExcelUtils.setExcelFile(sPath, "Sheet1");
+	 
+	     //Hard coded values are used for Excel row & columns for now
+	     //In later chapters we will replace these hard coded values with varibales
+	     //This is the loop for reading the values of the column 3 (Action Keyword) row by row
+	     for (int iRow=1;iRow<=14;iRow++){
+	     //Storing the value of excel cell in sActionKeyword string variable
+	     String sActionKeyword = ExcelUtils.getCellData(iRow, 3);
+	 
+	     //Comparing the value of Excel cell with all the project keywords
+	     if(sActionKeyword.equals("openBrowser")){
+	                        //This will execute if the excel cell value is 'openBrowser'
+	     //Action Keyword is called here to perform action
+	     ActionKeyword.openBrowser();}
+	     else if(sActionKeyword.equals("navigate")){
+	     ActionKeyword.navigate();}
+	     /*else if(sActionKeyword.equals("click_MyAccount")){
+	     ActionKeyword.click_MyAccount();}*/
+	     else if(sActionKeyword.equals("click_Admin")){
+	     ActionKeyword.click_Admin();}
+	     else if(sActionKeyword.equals("click_Password")){
+	     ActionKeyword.click_Password();}
+	     else if(sActionKeyword.equals("Submit")){
+	     ActionKeyword.Submit();}
+	     else if(sActionKeyword.equals("Account")){
+		     ActionKeyword.Account();}
+	     else if(sActionKeyword.equals("Member_Payment")){
+		     ActionKeyword.Member_Payment();}
+	     else if(sActionKeyword.equals("Login_Recipint")){
+		     ActionKeyword.Login_Recipint();
+		     Thread.sleep(2000);}
+	     else if(sActionKeyword.equals("Login_RecipintName")){
+		     ActionKeyword.Login_RecipintName();}
+	     else if(sActionKeyword.equals("Amount")){
+		     ActionKeyword.Amount();}
+	     else if(sActionKeyword.equals("Description")){
+		     ActionKeyword.Description();}
+	     else if(sActionKeyword.equals("Submit2")){
+		     ActionKeyword.Submit2();
+		     Thread.sleep(2000);}
+	     else if(sActionKeyword.equals("Submit3")){
+		     ActionKeyword.Submit3();
+		     Thread.sleep(2000);
+		     } 
+	     else if (sActionKeyword.equals("LogOut")){
+	    	 ActionKeyword.LogOut();
+	     }
+	     else if(sActionKeyword.equals("closeBrowser")){
+	     ActionKeyword.closeBrowser();}
+	     /*else if(sActionKeyword.equals("click_Logout")){
+	     ActionKeyword.click_Logout();}
+	     else if(sActionKeyword.equals("closeBrowser")){
+	     ActionKeyword.closeBrowser();}*/
+	 
+	     }
+	     }
+	 }
+	 
+
+	
