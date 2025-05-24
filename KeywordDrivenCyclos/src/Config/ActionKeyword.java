9cef73c500c4706021b5d60f17311bf1fc411d85
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-05-24 10:37:18 +0530

    Update ActionKeyword.java

--- KeywordDrivenCyclos/src/Config/ActionKeyword.java
+++ KeywordDrivenCyclos/src/Config/ActionKeyword.java
@@ -10,6 +10,7 @@ public class ActionKeyword {
 	public static WebDriver driver;
 	
 	 public static void openBrowser(){ 
+		 
 	     System.setProperty("webdriver.chrome.driver", ".\\Softwares Required For Testing Batch\\chromedriver_win32\\chromedriver.exe");
 	     driver=new ChromeDriver();
 	     driver.manage().window().maximize();
